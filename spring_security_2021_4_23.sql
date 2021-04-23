024344f4c4c5e87660a55c9936ab57daa29a45fd
xt962464 <2513669400@qq.com>
2021-04-23 23:08:02 +0800

    更新数据库

---
+++ spring_security_2021_4_23.sql
@@ -0,0 +1,165 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 50734
+ Source Host           : localhost:3306
+ Source Schema         : spring_security
+
+ Target Server Type    : MySQL
+ Target Server Version : 50734
+ File Encoding         : 65001
+
+ Date: 23/04/2021 22:28:41
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for sys_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_permission`;
+CREATE TABLE `sys_permission`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `permission_code` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '权限code',
+  `permission_name` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '权限名',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '权限表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_permission
+-- ----------------------------
+INSERT INTO `sys_permission` VALUES (1, 'admin_create', 'admin创建');
+INSERT INTO `sys_permission` VALUES (2, 'admin_query', 'admin查看');
+INSERT INTO `sys_permission` VALUES (3, 'admin_delete', 'admin删除');
+INSERT INTO `sys_permission` VALUES (4, 'admin_modify', 'admin修改');
+INSERT INTO `sys_permission` VALUES (5, 'user_create', '用户创建');
+INSERT INTO `sys_permission` VALUES (6, 'user_query', '用户查询');
+INSERT INTO `sys_permission` VALUES (7, 'user_delete', '用户删除');
+INSERT INTO `sys_permission` VALUES (8, 'user_modify', 'user_修改');
+
+-- ----------------------------
+-- Table structure for sys_request_path
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_request_path`;
+CREATE TABLE `sys_request_path`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `url` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '请求路径',
+  `description` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '路径描述',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '请求路径' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_request_path
+-- ----------------------------
+INSERT INTO `sys_request_path` VALUES (1, '/user/getUser', '查询用户');
+INSERT INTO `sys_request_path` VALUES (2, '/admin/info', 'admin');
+
+-- ----------------------------
+-- Table structure for sys_request_path_permission_relation
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_request_path_permission_relation`;
+CREATE TABLE `sys_request_path_permission_relation`  (
+  `id` int(11) NULL DEFAULT NULL COMMENT '主键id',
+  `url_id` int(11) NULL DEFAULT NULL COMMENT '请求路径id',
+  `permission_id` int(11) NULL DEFAULT NULL COMMENT '权限id'
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '路径权限关联表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_request_path_permission_relation
+-- ----------------------------
+INSERT INTO `sys_request_path_permission_relation` VALUES (1, 1, 5);
+INSERT INTO `sys_request_path_permission_relation` VALUES (4, 2, 1);
+
+-- ----------------------------
+-- Table structure for sys_role
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role`;
+CREATE TABLE `sys_role`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `role_name` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '角色名',
+  `role_description` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '角色说明',
+  `role_code` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户角色表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_role
+-- ----------------------------
+INSERT INTO `sys_role` VALUES (1, '管理员', '管理员，拥有所有权限', 'admin');
+INSERT INTO `sys_role` VALUES (2, '普通用户', '普通用户，拥有部分权限', 'user');
+
+-- ----------------------------
+-- Table structure for sys_role_permission_relation
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role_permission_relation`;
+CREATE TABLE `sys_role_permission_relation`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `role_id` int(11) NULL DEFAULT NULL COMMENT '角色id',
+  `permission_id` int(11) NULL DEFAULT NULL COMMENT '权限id',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 13 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '角色-权限关联关系表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_role_permission_relation
+-- ----------------------------
+INSERT INTO `sys_role_permission_relation` VALUES (1, 1, 1);
+INSERT INTO `sys_role_permission_relation` VALUES (2, 1, 2);
+INSERT INTO `sys_role_permission_relation` VALUES (3, 1, 3);
+INSERT INTO `sys_role_permission_relation` VALUES (4, 1, 4);
+INSERT INTO `sys_role_permission_relation` VALUES (5, 1, 5);
+INSERT INTO `sys_role_permission_relation` VALUES (6, 1, 6);
+INSERT INTO `sys_role_permission_relation` VALUES (7, 1, 7);
+INSERT INTO `sys_role_permission_relation` VALUES (8, 1, 8);
+INSERT INTO `sys_role_permission_relation` VALUES (9, 2, 5);
+INSERT INTO `sys_role_permission_relation` VALUES (10, 2, 6);
+INSERT INTO `sys_role_permission_relation` VALUES (11, 2, 7);
+INSERT INTO `sys_role_permission_relation` VALUES (12, 2, 8);
+
+-- ----------------------------
+-- Table structure for sys_user
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user`;
+CREATE TABLE `sys_user`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `account` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '账号',
+  `user_name` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户名',
+  `password` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户密码',
+  `last_login_time` datetime NULL DEFAULT NULL COMMENT '上一次登录时间',
+  `enabled` tinyint(1) NULL DEFAULT 1 COMMENT '账号是否可用。默认为1（可用）',
+  `account_not_expired` tinyint(1) NULL DEFAULT 1 COMMENT '是否过期。默认为1（没有过期）',
+  `account_not_locked` tinyint(1) NULL DEFAULT 1 COMMENT '账号是否锁定。默认为1（没有锁定）',
+  `credentials_not_expired` tinyint(1) NULL DEFAULT 1 COMMENT '证书（密码）是否过期。默认为1（没有过期）',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime NULL DEFAULT NULL COMMENT '修改时间',
+  `create_user` int(11) NULL DEFAULT NULL COMMENT '创建人',
+  `update_user` int(11) NULL DEFAULT NULL COMMENT '修改人',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_user
+-- ----------------------------
+INSERT INTO `sys_user` VALUES (1, 'admin', '管理员', '$2a$10$y4O8z2ZXs5fSkV/SGSwyAuXlXp716ZelkNS01hJlqHuXviX2kUdd2', '2021-04-23 13:14:43', 1, 1, 1, 1, '2019-08-29 06:28:36', '2021-04-23 13:14:43', 1, 1);
+INSERT INTO `sys_user` VALUES (2, 'user', '用户', '$2a$10$y4O8z2ZXs5fSkV/SGSwyAuXlXp716ZelkNS01hJlqHuXviX2kUdd2', '2021-04-23 12:56:19', 1, 1, 1, 1, '2019-08-29 06:29:24', '2021-04-23 12:56:19', 1, 2);
+
+-- ----------------------------
+-- Table structure for sys_user_role_relation
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user_role_relation`;
+CREATE TABLE `sys_user_role_relation`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `user_id` int(11) NULL DEFAULT NULL COMMENT '用户id',
+  `role_id` int(11) NULL DEFAULT NULL COMMENT '角色id',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户角色关联关系表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_user_role_relation
+-- ----------------------------
+INSERT INTO `sys_user_role_relation` VALUES (1, 1, 1);
+INSERT INTO `sys_user_role_relation` VALUES (2, 2, 2);
+
+SET FOREIGN_KEY_CHECKS = 1;
